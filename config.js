14ef77d3748b5a8a5ae3ed6cd7aa2a78ea7eec0b
Yuliia Kandych <yuliiakandych@Yuliia-DIPLY-0138-25067.local>
2019-12-08 16:23:34 -0500

    changes

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 8889, // 8889 for mac
+    uname: "root",
+    pword: "root", // leave blank for windows
+    database: ""
+}
+
+module.exports = config;
---
+++ gulp.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+
+function compile(done)
+{
+    gulp.src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("css"))
+
+}
+
+exports.compile = compile
