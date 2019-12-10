2e88972b8f33f90a839a714c0856784a27b741b1
Yuliia Kandych <yuliiakandych@YuliiaD13825067.fanshawec.ca>
2019-12-10 15:57:25 -0500

    bugging

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+onst express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + '/views'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', require('./routes/index.js'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req, res, next) => {
+  var err = new Error ('Not Found');
+  err.status = 404;
+  err.customMessage = "Ooops!";
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(err);
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  res.render('error', {data: err, layout: 'errorPage'});
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(port, ()=>{
+  console.log(`app is runng on ${port}`);
+})
 
-module.exports = app;
+
+
+
+
+
+
